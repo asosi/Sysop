55e153abcfd1f945a6e5f8926ac3369e08fe6e18
CaoS <sosi.andrea@gmail.com>
2013-05-14 11:57:44 +0200

    Modifica file plive.c

--- Plive/plive.c
+++ Plive/plive.c
@@ -1,8 +1,45 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
+void printdir(char *dir, int depth){
 
-int main(int argc, char const *argv[])
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    if((dp = opendir(dir)) == NULL) {
+        //fprintf(stderr,"cannot open directory: %s\n", dir);
+        perror(dir);
+        return;
+    }
+    chdir(dir);
+
+    while((entry = readdir(dp)) != NULL) {
+        lstat(entry->d_name,&statbuf);
+        if(S_ISDIR(statbuf.st_mode)) {
+
+            /* Cerca una directory e ignora . e .. */
+            if(strcmp(".",entry->d_name) == 0 || strcmp("..",entry->d_name) == 0){
+            	continue;
+            }
+
+            printf("%*s%s/\n",depth,"",entry->d_name);
+            /* Ricorsione con tabulazione */
+            printdir(entry->d_name,depth+4);
+        }
+        else printf("%*s%s\n",depth,"",entry->d_name);
+    }
+    chdir("..");
+    closedir(dp);
+}
+
+int main()
 {
-	/* code */
-	return 0;
+    printf("Sto guardando nella directory: ./:\n");
+    printdir("/Users/Andrea/Desktop",0);
+    printf("FINE!\n");
+    exit(0);
 }
